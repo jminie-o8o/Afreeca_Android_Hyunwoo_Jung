facda9ffd76e653d50745a3994319fdf14c58e61
hwj <jminie@naver.com>
2023-01-04 19:22:32 +0900

    feat: TalkCamFragment에 데이터바인딩 설정

--- app/src/main/java/com/example/afreecaandroid/ui/TalkCamFragment.kt
+++ app/src/main/java/com/example/afreecaandroid/ui/TalkCamFragment.kt
@@ -5,15 +5,25 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import com.example.afreecaandroid.R
+import com.example.afreecaandroid.databinding.FragmentTalkCamBinding
 
 class TalkCamFragment : Fragment() {
+
+    private var _binding: FragmentTalkCamBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_talk_cam, container, false)
+    ): View {
+        _binding = DataBindingUtil.inflate(inflater, R.layout.fragment_talk_cam, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }

9ad209c670186ca64638b6e149f251dc7cf0e2e2
hwj <jminie@naver.com>
2023-01-04 21:27:33 +0900

    feat: TalkCamRepository와 TalkCamDataSource 바인딩

--- app/src/main/java/com/example/afreecaandroid/data/datasource/TalkCamDataSourceImpl.kt
+++ app/src/main/java/com/example/afreecaandroid/data/datasource/TalkCamDataSourceImpl.kt
@@ -3,10 +3,13 @@ package com.example.afreecaandroid.data.datasource
 import androidx.paging.PagingData
 import com.example.afreecaandroid.data.model.TalkCamApiDTO
 import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
-class TalkCamDataSourceImpl: TalkCamDataSource {
+class TalkCamDataSourceImpl @Inject constructor(
+
+): TalkCamDataSource {
 
     override fun getTalkCamBroadCastList(): Flow<PagingData<TalkCamApiDTO>> {
         TODO("Not yet implemented")
--- app/src/main/java/com/example/afreecaandroid/data/repository/TalkCamRepositoryImpl.kt
+++ app/src/main/java/com/example/afreecaandroid/data/repository/TalkCamRepositoryImpl.kt
@@ -1,12 +1,16 @@
 package com.example.afreecaandroid.data.repository
 
 import androidx.paging.PagingData
+import com.example.afreecaandroid.data.datasource.TalkCamDataSource
 import com.example.afreecaandroid.ui.model.TalkCamData
 import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
-class TalkCamRepositoryImpl: TalkCamRepository {
+class TalkCamRepositoryImpl @Inject constructor(
+    private val talkCamDataSource: TalkCamDataSource
+): TalkCamRepository {
 
     override fun getTalkCamBroadCastList(): Flow<PagingData<TalkCamData>> {
         TODO("Not yet implemented")

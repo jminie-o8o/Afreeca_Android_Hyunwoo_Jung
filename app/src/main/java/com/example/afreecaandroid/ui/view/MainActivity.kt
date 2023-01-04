2cbaf89931ee557bbc9148ac516c4e9096efdd52
hwj <jminie@naver.com>
2023-01-04 19:33:27 +0900

    feat: ViewPager 와 MainActivity 연결

--- app/src/main/java/com/example/afreecaandroid/ui/view/MainActivity.kt
+++ app/src/main/java/com/example/afreecaandroid/ui/view/MainActivity.kt
@@ -3,10 +3,27 @@ package com.example.afreecaandroid.ui.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.afreecaandroid.R
+import com.example.afreecaandroid.databinding.ActivityMainBinding
+import com.example.afreecaandroid.ui.adapter.ViewPagerAdapter
+import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    private fun bindTabLayoutAtFragment() {
+        binding.viewPager.adapter = ViewPagerAdapter(this)
+        TabLayoutMediator(binding.tabLayout, binding.viewPager) { tab, position ->
+            when (position) {
+                0 -> tab.text = "토크/캠방"
+                1 -> tab.text = "여행"
+                2 -> tab.text = "먹방/쿡방"
+            }
+        }.attach()
     }
 }

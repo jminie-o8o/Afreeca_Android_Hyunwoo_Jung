88856b7aa35e834bc0172b4eec8660be3c817cd2
hwj <jminie@naver.com>
2023-01-04 19:23:43 +0900

    feat: TravelFragment에 데이터바인딩 설정

--- app/src/main/java/com/example/afreecaandroid/ui/TravelFragment.kt
+++ app/src/main/java/com/example/afreecaandroid/ui/TravelFragment.kt
@@ -5,15 +5,26 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import com.example.afreecaandroid.R
+import com.example.afreecaandroid.databinding.FragmentTalkCamBinding
+import com.example.afreecaandroid.databinding.FragmentTravelBinding
 
 class TravelFragment : Fragment() {
+
+    private var _binding: FragmentTravelBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_travel, container, false)
+        _binding = DataBindingUtil.inflate(inflater, R.layout.fragment_travel, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }

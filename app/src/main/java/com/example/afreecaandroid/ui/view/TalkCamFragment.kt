96a73a6b2048c6e02a026990da057975cbdd54f0
hwj <jminie@naver.com>
2023-01-05 13:56:50 +0900

    feat: TalkCamFragment 에서 UiState에 따라 데이터 처리 구현

--- app/src/main/java/com/example/afreecaandroid/ui/view/TalkCamFragment.kt
+++ app/src/main/java/com/example/afreecaandroid/ui/view/TalkCamFragment.kt
@@ -5,8 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.viewModels
+import androidx.paging.LoadState
 import androidx.paging.PagingDataAdapter
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -14,6 +16,8 @@ import com.example.afreecaandroid.R
 import com.example.afreecaandroid.databinding.FragmentTalkCamBinding
 import com.example.afreecaandroid.ui.adapter.TalkCamPagingAdapter
 import com.example.afreecaandroid.ui.viewmodel.TalkCamViewModel
+import com.example.afreecaandroid.uitl.UiState
+import com.example.afreecaandroid.uitl.collectLatestStateFlow
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -36,6 +40,8 @@ class TalkCamFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         talkCamPagingAdapter = TalkCamPagingAdapter()
         setupRecyclerView(talkCamPagingAdapter)
+        updateSearchWord()
+        showListEmptyText()
     }
 
     private fun setupRecyclerView(talkCamPagingAdapter: TalkCamPagingAdapter) {
@@ -48,6 +54,46 @@ class TalkCamFragment : Fragment() {
                     DividerItemDecoration.VERTICAL
                 )
             )
+        }
+    }
+
+    private fun updateSearchWord() {
+        collectLatestStateFlow(talkCamViewModel.talkCamBroadCastList) { uiState ->
+            when(uiState) {
+                is UiState.Loading -> {
+                    with(binding) {
+                        progressBar.isVisible = true
+                        tvEmptylist.isVisible = false
+                        rvTalkCam.isVisible = false
+                    }
+                }
+                is UiState.Error -> {
+                    with(binding) {
+                        progressBar.isVisible = false
+                        tvEmptylist.isVisible = true
+                        rvTalkCam.isVisible = false
+                    }
+                }
+                is UiState.Success -> {
+                    with(binding) {
+                        progressBar.isVisible = false
+                        tvEmptylist.isVisible = false
+                        rvTalkCam.isVisible = true
+                    }
+                    talkCamPagingAdapter.submitData(uiState.data)
+                }
+            }
+        }
+    }
+
+    private fun showListEmptyText() {
+        talkCamPagingAdapter.addLoadStateListener { combinedLoadStates ->
+            val loadState = combinedLoadStates.source
+            val isListEmpty = talkCamPagingAdapter.itemCount < 1
+                    && loadState.refresh is LoadState.NotLoading
+                    && loadState.append.endOfPaginationReached
+
+            binding.tvEmptylist.isVisible = isListEmpty
         }
     }
 

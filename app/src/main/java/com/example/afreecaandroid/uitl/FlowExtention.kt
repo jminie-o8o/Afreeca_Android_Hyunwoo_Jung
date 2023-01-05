6bcae48df3ce6bd90bae30e7cbc5d1655a602025
hwj <jminie@naver.com>
2023-01-05 13:47:05 +0900

    feat: repeatOnLifecycle을 이용해서 StateFlow 데이터 옵저빙하는 확장함수 구현

---
+++ app/src/main/java/com/example/afreecaandroid/uitl/FlowExtention.kt
@@ -0,0 +1,17 @@
+package com.example.afreecaandroid.uitl
+
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
+
+fun <T> Fragment.collectLatestStateFlow(flow: Flow<T>, collector: suspend (T) -> Unit) {
+    viewLifecycleOwner.lifecycleScope.launch {
+        viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+            flow.collectLatest(collector)
+        }
+    }
+}
